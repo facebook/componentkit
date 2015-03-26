019e0beffb102953fbb4ac15a73ee0fe8b86e071
Dustin Barker <dstnbrkr@fb.com>
2015-03-26 11:47:28 -0700

    Add appledoc task to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -eu
+
+MODE=$1
+
 function ci() {
   pod install
   xctool \
@@ -10,15 +14,36 @@ function ci() {
       $2
 }
 
-set -e
+if [ "$MODE" = "ci" ]; then
+  ci ComponentKit test
 
-ci ComponentKit test
+  pushd Examples/WildeGuess
+  ci WildeGuess build
+  popd
+fi
 
-pushd Examples/WildeGuess
-ci WildeGuess build
-popd
+if [ "$MODE" = "docs" ]; then
+  HEADERS=`ls ComponentKit/**/*.h ComponentTextKit/**/*.h`
+  rm -rf appledoc
 
-
-
-
-
+  appledoc \
+    --no-create-docset \
+    --create-html \
+    --exit-threshold 2 \
+    --no-repeat-first-par \
+    --no-merge-categories \
+    --explicit-crossref \
+    --warn-missing-output-path \
+    --warn-missing-company-id \
+    --warn-undocumented-object \
+    --warn-undocumented-member \
+    --warn-empty-description \
+    --warn-unknown-directive \
+    --warn-invalid-crossref \
+    --warn-missing-arg \
+    --project-name ComponentKit \
+    --project-company Facebook \
+    --company-id "org.componentkit" \
+    --output appledoc \
+    $HEADERS
+fi
