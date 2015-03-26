135084b478c22d7998bcab92b0d2a5239d43e8c6
Oliver Clark Rickard <ocrickard@fb.com>
2015-03-25 20:09:52 -0400

    [componentkit] Fix TextKit build issues in Xcode 6.3
    This didn’t need to be const and the new version of the compiler was
    complaining.

--- ComponentTextKit/TextKit/CKTextKitRendererCache.h
+++ ComponentTextKit/TextKit/CKTextKitRendererCache.h
@@ -66,7 +66,7 @@ namespace CK {
         CKTextKitAttributes attributes;
         CGSize constrainedSize;
 
-        const Key(CKTextKitAttributes a, CGSize cs);
+        Key(CKTextKitAttributes a, CGSize cs);
 
         size_t hash;
 
