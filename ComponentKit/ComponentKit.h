d3e5a427b65b792df085899065667c4c37cfbddb
Jose Luis Piedrahita <jlpiedrahita@gmail.com>
2015-04-09 20:23:32 +0200

    Add umbrella header

---
+++ ComponentKit/ComponentKit.h
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+//Accessibility
+#import <ComponentKit/CKComponentAccessibility.h>
+//Components
+#import <ComponentKit/CKButtonComponent.h>
+#import <ComponentKit/CKImageComponent.h>
+#import <ComponentKit/CKNetworkImageComponent.h>
+#import <ComponentKit/CKNetworkImageDownloading.h>
+//Core
+#import <ComponentKit/CKComponent.h>
+#import <ComponentKit/CKComponentAnimation.h>
+#import <ComponentKit/CKComponentAnimationHooks.h>
+#import <ComponentKit/CKComponentBoundsAnimation.h>
+#import <ComponentKit/CKComponentController.h>
+#import <ComponentKit/CKComponentLayout.h>
+#import <ComponentKit/CKComponentLifecycleManager.h>
+#import <ComponentKit/CKComponentLifecycleManagerAsynchronousUpdateHandler.h>
+#import <ComponentKit/CKComponentSize.h>
+#import <ComponentKit/CKComponentViewAttribute.h>
+#import <ComponentKit/CKComponentViewConfiguration.h>
+#import <ComponentKit/CKCompositeComponent.h>
+#import <ComponentKit/CKDimension.h>
+#import <ComponentKit/CKComponentScope.h>
+//Datasources
+#import <ComponentKit/CKCollectionViewDataSource.h>
+#import <ComponentKit/CKComponentConstantDecider.h>
+#import <ComponentKit/CKComponentDataSource.h>
+#import <ComponentKit/CKComponentDataSourceOutputItem.h>
+#import <ComponentKit/CKComponentDeciding.h>
+#import <ComponentKit/CKComponentPreparationQueueListener.h>
+#import <ComponentKit/CKComponentPreparationQueueTypes.h>
+#import <ComponentKit/CKComponentProvider.h>
+//HostingView
+#import <ComponentKit/CKComponentFlexibleSizeRangeProvider.h>
+#import <ComponentKit/CKComponentHostingView.h>
+#import <ComponentKit/CKComponentHostingViewDelegate.h>
+#import <ComponentKit/CKComponentRootView.h>
+#import <ComponentKit/CKComponentSizeRangeProviding.h>
+//LayoutComponents
+#import <ComponentKit/CKBackgroundLayoutComponent.h>
+#import <ComponentKit/CKCenterLayoutComponent.h>
+#import <ComponentKit/CKInsetComponent.h>
+#import <ComponentKit/CKOverlayLayoutComponent.h>
+#import <ComponentKit/CKRatioLayoutComponent.h>
+#import <ComponentKit/CKStackLayoutComponent.h>
+#import <ComponentKit/CKStaticLayoutComponent.h>
+//Utilities
+#import <ComponentKit/CKArrayControllerChangeset.h>
+#import <ComponentKit/CKArrayControllerChangeType.h>
+#import <ComponentKit/CKComponentAction.h>
+#import <ComponentKit/CKComponentContext.h>
+#import <ComponentKit/CKComponentGestureActions.h>
+#import <ComponentKit/CKOptimisticViewMutations.h>
+#import <ComponentKit/CKSectionedArrayController.h>
+//Text
+#import <ComponentKit/CKLabelComponent.h>
+#import <ComponentKit/CKTextComponent.h>
+#import <ComponentKit/CKTextKitAttributes.h>
