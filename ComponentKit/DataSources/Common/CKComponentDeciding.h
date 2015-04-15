6dfa9b59374fbc18a8a3a03b2b25311cd5ef952b
Benjamin Loulier <benloulier@fb.com>
2015-04-15 14:00:50 -0700

    Remove reason from CKComponentDeciding

--- ComponentKit/DataSources/Common/CKComponentDeciding.h
+++ ComponentKit/DataSources/Common/CKComponentDeciding.h
@@ -18,10 +18,4 @@
  */
 - (id)componentCompliantModel:(id)model;
 
-/*
- * In case the model is not component compliant, returns a string explaining why
- * Otherwise returns nil. Used for logging and debugging.
- */
-- (NSString *)componentComplianceReason:(id)model;
-
 @end
