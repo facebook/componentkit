0f4c6278fa9f98f680782276da4fd919ba115faf
Jonathan Crooke <joncrooke@gmail.com>
2015-10-04 19:33:33 +0200

    Add test root view controller

--- ComponentKitApplicationTestsHost/ComponentKitApplicationTestsHostAppDelegate.m
+++ ComponentKitApplicationTestsHost/ComponentKitApplicationTestsHostAppDelegate.m
@@ -17,6 +17,7 @@
 #pragma unused(application)
 #pragma unused(launchOptions)
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  self.window.rootViewController = [UIViewController new];
   // Override point for customization after application launch.
   self.window.backgroundColor = [UIColor whiteColor];
   [self.window makeKeyAndVisible];
