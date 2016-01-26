285b89e8e0e2493874ff3a9ab482a1ec5c833ff0
dbworku <daniel.worku@fieldnation.com>
2016-01-26 09:48:09 -0600

    Avoid CKComponentAnimation overrides
    - Use single function with a default value of nil for the layerPath
    parameter to avoid overrides.
    - Fix pointer spacing
    - Expand `CKComponentAnimation` documentation to clarify usage of a
    custom layerPath.

--- ComponentKit/Core/CKComponentAnimation.h
+++ ComponentKit/Core/CKComponentAnimation.h
@@ -18,26 +18,23 @@
 struct CKComponentAnimation {
 
   /**
-   Creates a CKComponentAnimation that applies a CAAnimation to a CKComponent on the layer found at layerPath.
+   Creates a CKComponentAnimation that applies a CAAnimation to a CKComponent, on the layer found at layerPath. The
+   layer of the view is used when layerPath is nil.
    
    @note The CKComponent must create a UIView via its view configuration; or, it must be a CKCompositeComponent that
-   renders to a component that creates a view. The view must have a CALayer at the keypath that corresponds to 
-   layerPath.
-   
-   @example Suppose the component has a CAShapeLayer as the mask of the view's layer. The path of the mask could be animated
-   with: {myComponent, [CABasicAnimation animationWithKeypath:@"path"], "layer.mask"}
-   */
-  CKComponentAnimation(CKComponent* component, CAAnimation* animation, NSString* layerPath);
-  
-  /**
-   Creates a CKComponentAnimation that applies a CAAnimation to a CKComponent on the layer of the view.
-   
-   @example {myComponent, [CABasicAnimation animationWithKeypath:@"position"]}
-   
-   @see CKComponentAnimation(CKComponent* component, CAAnimation* animation, NSString* layerPath)
-   */
-  CKComponentAnimation(CKComponent *component, CAAnimation *animation);
+   renders to a component that creates a view. The view must have a CALayer at the keypath that corresponds to
+   layerPath; or, it must be nil.
 
+   @example Animate the position of a component: {myComponent, [CABasicAnimation animationWithKeypath:@"position"]}
+   
+   @example Suppose the component has a CAShapeLayer as the mask of the view's layer. The path of the mask could be
+   animated with: {myComponent, [CABasicAnimation animationWithKeypath:@"path"], "layer.mask"}
+   
+   @param component A CKComponent that will be animated.
+   @param animation A CAAnimation to apply on the component's layer.
+   @param layerPath A key path to a sublayer of the component's view. Defaults to nil.
+   */
+  CKComponentAnimation(CKComponent *component, CAAnimation *animation, NSString *layerPath = nil);
   
   /** Creates a completely custom animation with arbitrary hooks. */
   CKComponentAnimation(const CKComponentAnimationHooks &hooks);
