5cb21e3b4905be64b9c7ccb673eff4c5a18fc43a
Oliver Rickard <ocrickard@gmail.com>
2016-11-23 10:07:47 -0500

    add copyright header

--- ComponentKit/Components/CKTestTypedPropsComponent.mm
+++ ComponentKit/Components/CKTestTypedPropsComponent.mm
@@ -1,10 +1,12 @@
-//
-//  CKTestTypedPropsComponent.m
-//  ComponentKit
-//
-//  Created by Oliver Rickard on 11/23/16.
-//
-//
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
 
 #import "CKTestTypedPropsComponent.h"
 
