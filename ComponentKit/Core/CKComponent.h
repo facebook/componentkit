8f4a55ce6824b61479c813b98053b4277991ada5
Dustin Shahidehpour <dust@fb.com>
2017-04-26 12:44:04 -0400

    Fix duplicate import

--- ComponentKit/Core/CKComponent.h
+++ ComponentKit/Core/CKComponent.h
@@ -16,7 +16,6 @@
 
 #import <ComponentKit/CKComponentSize.h>
 #import <ComponentKit/CKComponentViewConfiguration.h>
-#import <ComponentKit/CKScopedComponent.h>
 
 struct CKComponentViewContext {
   __kindof UIView *view;
@@ -24,7 +23,7 @@ struct CKComponentViewContext {
 };
 
 /** A component is an immutable object that specifies how to configure a view, loosely inspired by React. */
-@interface CKComponent : NSObject <CKScopedComponent>
+@interface CKComponent : NSObject
 
 /**
  @param view A struct describing the view for this component. Pass {} to specify that no view should be created.
