07154352a063b77c0fe94cdfb753c31a9743cc3a
Adam Ernst <adamjernst@fb.com>
2017-06-14 19:24:24 -0400

    Simplify intersperse
    No one uses the predicate, so remove it.

--- ComponentKit/Core/ComponentUtilities.h
+++ ComponentKit/Core/ComponentUtilities.h
@@ -82,28 +82,20 @@ namespace CK {
    inputs: { 1, 2, 3, 4, 5, 6 }, factory ^(int) { return 0; } predicate: ^BOOL(int a) { return a%2 == 0; }
    output: { 2, 0, 4, 0, 6 }
    */
-  template <typename T, typename Func, typename Predicate>
-  auto intersperse(std::vector<T> a, Func &&factory, Predicate predicate) -> std::vector<T>
+  template <typename T, typename Func>
+  auto intersperse(const std::vector<T> &a, Func &&factory) -> std::vector<T>
   {
-    std::vector<T> newVector;
-
     if (a.size() < 2) {
-      return (a.size() == 1 && predicate(a.at(0))) ? a : newVector;
+      return a;
     }
 
-    for (int i = 0; i < a.size() - 1; i++) {
-      T currentA = a.at(i);
-      if (predicate(currentA)) {
-        newVector.push_back(currentA);
+    std::vector<T> newVector;
+    for (int i = 0; i < a.size(); i++) {
+      newVector.push_back(a.at(i));
+      if (i != a.size() - 1) {
         newVector.push_back(factory());
       }
     }
-
-    T lastA = a.at(a.size() - 1);
-    if (predicate(lastA)) {
-      newVector.push_back(lastA);
-    }
-
     return newVector;
   }
 };
