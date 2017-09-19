dd81d8e93c212b859818200677492bf8e474231f
Georgiy Kassabli <gkassabli@fb.com>
2017-09-19 11:11:45 -0700

    Moving calculation of DataSourceAppliedChanges index transitions to CKTransactionalComponentDataSourceAppliedChanges property
    Summary: This diff moves calculation of DataSourceAppliedChanges index transitions to CKTransactionalComponentDataSourceAppliedChanges lazily-initialized property. This is done in order to pave way for reusing them in DataSource for calling controller update events
    
    Reviewed By: aCorrado
    
    Differential Revision: D5825473
    
    fbshipit-source-id: d0f5639aefc96f31972b2af7ae4692e19af2f94c

--- ComponentKit/TransactionalDataSources/Common/CKTransactionalComponentDataSourceAppliedChanges.h
+++ ComponentKit/TransactionalDataSources/Common/CKTransactionalComponentDataSourceAppliedChanges.h
@@ -22,6 +22,30 @@
 /** userInfo from the CKTransactionalComponentDataSourceChangeset object that caused this change. */
 @property (nonatomic, copy, readonly) NSDictionary *userInfo;
 
+/**
+ This property returns a mapping for each row being updated
+ from their current index path to their new index path after the changeset has been applied.
+ It is lazily initialized and cached, so the calculation occures only once if needed
+ 
+ Because this is pretty confusing, here's a simple example:
+ Let's say we have a changeset made up of the following operations:
+ Insert a row at (0, 0)
+ Update the row at (0, 1)
+ 
+ Feeding this changeset into the function below would return:
+ (0,1) -> (0,2)
+ 
+ This mapping occurs because the inserted row at (0,0) pushes the row at (0,1) down one row.
+ 
+ Of course, these changesets can be more and more complicated, so updated rows can move up or down, and change rows or sections.
+ 
+ There are also certain assumptions made:
+ - An updated row cannot also be removed, both in terms of row removal and section removal (if it's removed, we ignore it and continue as normal)
+ - A section cannot be inserted and removed in the same changeset
+ - A row cannot be inserted and removed in the same changeset
+ - A row cannot be moved and removed in the same changeset
+ */
+@property (nonatomic, readonly) NSDictionary<NSIndexPath *, NSIndexPath *>* finalUpdatedIndexPaths;
 
 /** Any of the parameters may be nil, in which case a default value will be substituted instead. */
 - (instancetype)initWithUpdatedIndexPaths:(NSSet *)updatedIndexPaths
--- ComponentKit/TransactionalDataSources/Common/CKTransactionalComponentDataSourceAppliedChanges.mm
+++ ComponentKit/TransactionalDataSources/Common/CKTransactionalComponentDataSourceAppliedChanges.mm
@@ -9,13 +9,16 @@
  */
 
 #import "CKTransactionalComponentDataSourceAppliedChanges.h"
-#import "CKTransactionalComponentDataSourceAppliedChanges.h"
 
 #import "ComponentUtilities.h"
 #import "CKEqualityHashHelpers.h"
 #import "CKMacros.h"
 
-@implementation CKTransactionalComponentDataSourceAppliedChanges
+
+@implementation CKTransactionalComponentDataSourceAppliedChanges {
+  NSDictionary<NSIndexPath *, NSIndexPath *>* _finalUpdatedIndexPaths;
+  dispatch_once_t _finalUpdatedPathsToken;
+}
 
 - (instancetype)init
 {
@@ -86,6 +89,104 @@
     [_userInfo hash],
   };
   return CKIntegerArrayHash(subhashes, CK_ARRAY_COUNT(subhashes));
+}
+
+static NSArray *sortedIndexPaths(NSArray<NSIndexPath *> *indexPaths, BOOL reverse)
+{
+  return [indexPaths sortedArrayUsingComparator:^NSComparisonResult(NSIndexPath *_Nonnull obj1, NSIndexPath *_Nonnull obj2) {
+    NSComparisonResult sectionComparison = [@(obj1.section) compare:@(obj2.section)];
+    if (sectionComparison != NSOrderedSame) {
+      return (NSComparisonResult)(sectionComparison * (reverse ? -1 : 1));
+    } else {
+      return (NSComparisonResult)([@(obj1.row) compare:@(obj2.row)] * (reverse ? -1 : 1));
+    }
+  }];
+}
+
+static NSIndexPath *indexPathWithDeltas(NSIndexPath *indexPath, NSUInteger sectionDelta, NSUInteger rowDelta)
+{
+  return [NSIndexPath indexPathForRow:(indexPath.row + rowDelta) inSection:(indexPath.section + sectionDelta)];
+}
+
+- (NSDictionary<NSIndexPath *, NSIndexPath *> *)finalUpdatedIndexPaths
+{
+  dispatch_once(&_finalUpdatedPathsToken, ^{
+    NSArray *updatedIndexPaths = [self.updatedIndexPaths allObjects];
+    
+    // The dictionary that will be returned will have the structure:
+    // (old update index path) -> (new update index path)
+    NSMutableDictionary<NSIndexPath *, NSIndexPath *> *finalUpdatedIndexPaths = [NSMutableDictionary new];
+    
+    // Initialize with updated index paths mapping to themselves
+    for (NSIndexPath *indexPath in [self.updatedIndexPaths allObjects]) {
+      finalUpdatedIndexPaths[indexPath] = indexPath;
+    }
+    
+    
+    // Translate moves into removals and insertions
+    NSArray *moveRemovals = [self.movedIndexPaths allKeys];
+    NSArray *moveInsertions = [self.movedIndexPaths allValues];
+    
+    // Removed rows
+    // Reverse sort (hence the -1 coefficient) so we don't end up in a situation like the following
+    // Updating (0, 5) and removing (0,2) (0,3) (0,4). Since the removed index paths are unordered (NSSet) we could end up in a situation where we remove (0,2) and (0,3), so (0,5) -> (0,3) and when we remove (0,4) the updated row is assumed unaffected
+    NSArray *allRemovals = [[self.removedIndexPaths allObjects] arrayByAddingObjectsFromArray:moveRemovals];
+    for (NSIndexPath *removedIndexPath in sortedIndexPaths(allRemovals, YES)) {
+      for (NSIndexPath *sourceUpdatePath in updatedIndexPaths) {
+        NSIndexPath *destinationUpdatePath = finalUpdatedIndexPaths[sourceUpdatePath];
+        if (removedIndexPath.section == destinationUpdatePath.section &&
+            removedIndexPath.row < destinationUpdatePath.row) {
+          finalUpdatedIndexPaths[sourceUpdatePath] = indexPathWithDeltas(destinationUpdatePath, 0, -1);
+        }
+      }
+    }
+    
+    // Removed sections
+    [self.removedSections enumerateIndexesWithOptions:NSEnumerationReverse usingBlock:^(NSUInteger removedSection, BOOL *_Nonnull removedSectionsStop) {
+      for (NSIndexPath *sourceUpdatePath in updatedIndexPaths) {
+        NSIndexPath *destinationUpdatePath = finalUpdatedIndexPaths[sourceUpdatePath];
+        if (removedSection < destinationUpdatePath.section) {
+          finalUpdatedIndexPaths[sourceUpdatePath] = indexPathWithDeltas(destinationUpdatePath, -1, 0);
+        }
+      }
+    }];
+    
+    // Inserted sections
+    [self.insertedSections enumerateIndexesUsingBlock:^(NSUInteger insertedSection, BOOL *_Nonnull insertedSectionsStop) {
+      for (NSIndexPath *sourceUpdatePath in updatedIndexPaths) {
+        NSIndexPath *destinationUpdatePath = finalUpdatedIndexPaths[sourceUpdatePath];
+        if (insertedSection <= destinationUpdatePath.section) {
+          finalUpdatedIndexPaths[sourceUpdatePath] = indexPathWithDeltas(destinationUpdatePath, 1, 0);
+        }
+      }
+    }];
+    
+    // Inserted rows
+    // Sort for the same reason as above when we sorted the removed index paths
+    // First take care of the "normal" insertions, where we check the destination update path
+    NSArray *allInsertions = [[self.insertedIndexPaths allObjects] arrayByAddingObjectsFromArray:moveInsertions];
+    for (NSIndexPath *insertedIndexPath in sortedIndexPaths(allInsertions, NO)) {
+      for (NSIndexPath *sourceUpdatePath in updatedIndexPaths) {
+        NSIndexPath *destinationUpdatePath = finalUpdatedIndexPaths[sourceUpdatePath];
+        if (insertedIndexPath.section == destinationUpdatePath.section &&
+            insertedIndexPath.row <= destinationUpdatePath.row) {
+          finalUpdatedIndexPaths[sourceUpdatePath] = indexPathWithDeltas(destinationUpdatePath, 0, 1);
+        }
+      }
+    }
+    
+    // The destination index path of a move is *always* correct and we have it take the precedence over all above.
+    // If one inserts a section at the front, even a move within an existing section
+    // has to provide the correct final section index.
+    [self.movedIndexPaths enumerateKeysAndObjectsUsingBlock:^(NSIndexPath *sourceIndexPath, NSIndexPath *destinationIndexPath, BOOL *_Nonnull stop) {
+      if (finalUpdatedIndexPaths[sourceIndexPath]) {
+        finalUpdatedIndexPaths[sourceIndexPath] = destinationIndexPath;
+      }
+    }];
+    
+    _finalUpdatedIndexPaths = finalUpdatedIndexPaths;
+  });
+  return _finalUpdatedIndexPaths;
 }
 
 @end
