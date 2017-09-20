784b173ad93021a16e0019fc49b3c272e6974e98
Georgiy Kassabli <gkassabli@fb.com>
2017-09-20 09:12:45 -0700

    Removing simulator-specific code frome xcodebuild script
    Summary: I want to remove simulator-specific code so that we don't have to correct it again for every new build
    
    Reviewed By: Andrey-Mishanin
    
    Differential Revision: D5871238
    
    fbshipit-source-id: c5f039c290833ec6853aa7361b60f2ac5c2ccf2e

--- build.sh
+++ build.sh
@@ -31,11 +31,11 @@ function ci() {
 }
 
 function ios_ci() {
-  ci $1 $2 iphonesimulator10.3 "platform=iOS Simulator,OS=10.3.1,name=iPhone 5s" $3
+  ci $1 $2 iphonesimulator "platform=iOS Simulator,name=iPhone 5s" $3
 }
 
 function tvos_ci() {
-  ci $1 $2 appletvsimulator10.2 "platform=tvOS Simulator,OS=10.2,name=Apple TV 1080p" $3
+  ci $1 $2 appletvsimulator "platform=tvOS Simulator,name=Apple TV" $3
 }
 
 if [ "$MODE" = "ci-componentkit-ios" ]; then
