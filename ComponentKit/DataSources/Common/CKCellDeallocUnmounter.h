9d9672f641311229088f4a8d2b849c7a57c6a9cb
Brennan Vincent <btv@fb.com>
2017-12-06 00:04:20 -0800

    Add function to unmount cell components on dealloc
    Summary:
    Add a function that will cause a particular component layout to be unmounted when a particular object is deallocated.
    
    This is intended to be used when the owner of some component hierarchy is deallocated and we want to make sure the components it holds on to are deallocated as well, to avoid leaking memory.
    
    Reviewed By: supercfanx
    
    Differential Revision: D6483978
    
    fbshipit-source-id: e80e8610c3f891f79f0bdd99ade88213cccb412a

---
+++ ComponentKit/DataSources/Common/CKCellDeallocUnmounter.h
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIView.h>
+
+#import <ComponentKit/CKComponentDataSourceAttachController.h>
+#import <ComponentKit/CKComponentScopeTypes.h>
+
+/*
+ * This will cause the layout referenced by `scopeIdentifier` to be unmounted when `cell` is deallocated.
+ * This is useful for collection views, whose cell views are managed by UIKit: usually they are re-used,
+ * but sometimes they are deallocated. At that point, the object graph looks like this:
+ 
+                                             Cell view
+                                              |
+                                              |
+       attachController                       v
+(via _scopeIdentifierToAttachedViewMap) --> root view
+                                              |
+                                              |
+                                              v
+                                             View <--> Component
+                                             / | \
+                                            /  |  \
+                                           /   |   v
+                                          /    |  View <--> Component
+                                         /     v
+                                        /    View <--> Component
+                                       v
+                                      View <--> Component
+
+ * The Cell --> RootView link is broken when the cell is deallocated. But everything else sticks around, wasting memory.
+ * Thus, we should unmount whatever was mounted to the cell's root view, which will cause the Map->RootView link
+ * and all the View <--> Component links to be broken and the whole structure to be freed.
+ */
+void CKSetupDeallocUnmounter(UIView *cell, CKComponentScopeRootIdentifier scopeIdentifier, CKComponentDataSourceAttachController *attachController);
---
+++ ComponentKit/DataSources/Common/CKCellDeallocUnmounter.mm
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import "CKCellDeallocUnmounter.h"
+
+@interface CKCellDeallocUnmounter : NSObject
+
+@property (nonatomic, assign) CKComponentScopeRootIdentifier scopeIdentifier;
+@property (nonatomic, weak) CKComponentDataSourceAttachController *attachController;
+
++ (CKCellDeallocUnmounter *)unmounterForCell:(UIView *)cell;
+@end
+
+@implementation CKCellDeallocUnmounter
+
+- (void)setScopeIdentifier:(CKComponentScopeRootIdentifier)scopeIdentifier
+{
+  CKAssertMainThread();
+  _scopeIdentifier = scopeIdentifier;
+}
+
+- (void)setAttachController:(CKComponentDataSourceAttachController *)attachController
+{
+  CKAssertMainThread();
+  _attachController = attachController;
+}
+
+- (void)dealloc
+{
+  CKAssertMainThread();
+  [_attachController detachComponentLayoutWithScopeIdentifier:_scopeIdentifier];
+}
+
++ (CKCellDeallocUnmounter *)unmounterForCell:(UIView *)cell
+{
+  static char kUnmounterKey;
+  CKCellDeallocUnmounter *unmounter = objc_getAssociatedObject(cell, &kUnmounterKey);
+  if (!unmounter) {
+    unmounter = [CKCellDeallocUnmounter new];
+    objc_setAssociatedObject(cell, &kUnmounterKey, unmounter, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+  }
+  return unmounter;
+}
+@end
+
+
+void CKSetupDeallocUnmounter(UIView *cell, CKComponentScopeRootIdentifier scopeIdentifier, CKComponentDataSourceAttachController *attachController)
+{
+  CKCellDeallocUnmounter *unmounter = [CKCellDeallocUnmounter unmounterForCell:cell];
+  unmounter.scopeIdentifier = scopeIdentifier;
+  unmounter.attachController = attachController;
+}
