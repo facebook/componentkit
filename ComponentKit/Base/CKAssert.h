658829e707adb81a4614ba3bc4f7569082e457f5
Daryl Johnas Sison <djsison@fb.com>
2018-01-09 09:50:22 -0800

    Fixed CK macros
    Summary:
    There is an extra "nil" in the expanded macro.
    I removed this so that the description will be passed as the actual description, not as varargs.
    
    Reviewed By: gkassabli
    
    Differential Revision: D6683425
    
    fbshipit-source-id: 439207aeefc6fd5398d8a4b41062c5649380eb33

--- ComponentKit/Base/CKAssert.h
+++ ComponentKit/Base/CKAssert.h
@@ -19,8 +19,8 @@
 #define CKAssert(condition, description, ...) NSAssert(condition, description, ##__VA_ARGS__)
 #define CKCAssert(condition, description, ...) NSCAssert(condition, description, ##__VA_ARGS__)
 
-#define CKConditionalAssert(shouldTestCondition, condition, description, ...) CKAssert((!(shouldTestCondition) || (condition)), nil, (description), ##__VA_ARGS__)
-#define CKCConditionalAssert(shouldTestCondition, condition, description, ...) CKCAssert((!(shouldTestCondition) || (condition)), nil, (description), ##__VA_ARGS__)
+#define CKConditionalAssert(shouldTestCondition, condition, description, ...) CKAssert((!(shouldTestCondition) || (condition)), (description), ##__VA_ARGS__)
+#define CKCConditionalAssert(shouldTestCondition, condition, description, ...) CKCAssert((!(shouldTestCondition) || (condition)), (description), ##__VA_ARGS__)
 
 #define CKAssertNil(condition, description, ...) CKAssert(!(condition), (description), ##__VA_ARGS__)
 #define CKCAssertNil(condition, description, ...) CKCAssert(!(condition), (description), ##__VA_ARGS__)
@@ -34,11 +34,11 @@
 #define CKAssertFalse(condition) CKAssert(!(condition), nil, nil)
 #define CKCAssertFalse(condition) CKCAssert(!(condition), nil, nil)
 
-#define CKAssertMainThread() CKAssert([NSThread isMainThread], nil, @"This method must be called on the main thread")
-#define CKCAssertMainThread() CKCAssert([NSThread isMainThread], nil, @"This method must be called on the main thread")
+#define CKAssertMainThread() CKAssert([NSThread isMainThread], @"This method must be called on the main thread")
+#define CKCAssertMainThread() CKCAssert([NSThread isMainThread], @"This method must be called on the main thread")
 
-#define CKFailAssert(description, ...) CKAssert(NO, nil, (description), ##__VA_ARGS__)
-#define CKCFailAssert(description, ...) CKCAssert(NO, nil, (description), ##__VA_ARGS__)
+#define CKFailAssert(description, ...) CKAssert(NO, (description), ##__VA_ARGS__)
+#define CKCFailAssert(description, ...) CKCAssert(NO, (description), ##__VA_ARGS__)
 
-#define CKFatal(description, ...) CKAssert(NO, nil, (description), ##__VA_ARGS__)
-#define CKCFatal(description, ...) CKCAssert(NO, nil, (description), ##__VA_ARGS__)
+#define CKFatal(description, ...) CKAssert(NO, (description), ##__VA_ARGS__)
+#define CKCFatal(description, ...) CKCAssert(NO, (description), ##__VA_ARGS__)
