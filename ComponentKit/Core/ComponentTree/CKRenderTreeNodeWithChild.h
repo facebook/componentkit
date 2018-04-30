01500706ff6b42185a7485d70134d301c9c2a94a
Kfir Schindelhaim <kfirs@fb.com>
2018-04-30 06:56:37 -0700

    Added CKRenderTreeNodeWithChild
    Summary: New tree node type for a single child component
    
    Reviewed By: aCorrado
    
    Differential Revision: D7790897
    
    fbshipit-source-id: 8e3f9da32feb891d4da6e4a69ef7714937fde065

---
+++ ComponentKit/Core/ComponentTree/CKRenderTreeNodeWithChild.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import "CKRenderTreeNode.h"
+#import "CKTreeNodeProtocol.h"
+
+/**
+ This object represents a node with a single child in the component tree.
+
+ Each CKRenderComponent will have a corresponding CKRenderTreeNodeWithChild (when we use parent based component key).
+ */
+
+@interface CKRenderTreeNodeWithChild : CKRenderTreeNode <CKTreeNodeWithChildrenProtocol>
+
+@end
---
+++ ComponentKit/Core/ComponentTree/CKRenderTreeNodeWithChild.mm
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import "CKRenderTreeNodeWithChild.h"
+
+#import <ComponentKit/CKComponent.h>
+#import <ComponentKit/CKComponentInternal.h>
+
+#include <tuple>
+
+@implementation CKRenderTreeNodeWithChild
+{
+  CKTreeNode *_child;
+}
+
+- (std::vector<id<CKTreeNodeProtocol>>)children
+{
+  return {_child};
+}
+
+- (CKTreeNode *)childForComponentKey:(const CKComponentKey &)key
+{
+  CKAssert(std::get<0>(key) == [_child.component class], @"CKComponentKey(%@, %ld) contains incorrect class(%@) type.",
+           std::get<0>(key),
+           (unsigned long)std::get<1>(key),
+           [_child.component class]);
+  return _child;
+}
+
+- (CKComponentKey)createComponentKeyForChildWithClass:(id<CKComponentProtocol>)componentClass
+{
+  return std::make_tuple(componentClass, 0);
+}
+
+- (void)setChild:(CKTreeNode *)child forComponentKey:(const CKComponentKey &)componentKey
+{
+  CKAssert(_child == nil, @"_child shouldn't set more than once.");
+  _child = child;
+}
+
+@end
