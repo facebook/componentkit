2c1007c7d39cabb94f1812f3d3b325713605b604
Andrey Mishanin <amishanin@fb.com>
2018-07-03 08:26:33 -0700

    Introducing component animations controller
    Summary:
    `CKComponentAnimationsController` represents a state of animations infrastructure during one mount operation, i.e. each mount pass creates a new instance of `CKComponentAnimationsController` if there any component animations. It is a spiritual successor to `CKAnimationController` and `CKComponentControllerAnimationData` (I'm even reusing some data structures from latter).
    
    Please, see test cases for a comprehensive description of `CKComponentAnimationsController` behaviour.
    
    Reviewed By: aCorrado, fabiomassimo
    
    Differential Revision: D8624763
    
    fbshipit-source-id: 7802de49aa95fcb0986b7a9e505f4c7b8f6a5dd1

---
+++ ComponentKit/Core/CKComponentAnimationsController.h
@@ -0,0 +1,104 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+
+#import <ComponentKit/CKComponentAnimationData.h>
+#import <ComponentKit/CKComponentAnimations.h>
+
+namespace CK {
+  struct CATransactionProvider
+  {
+    // Even though these lambdas are supposed to have identical signatures, formally speaking, actual
+    // anonymous function object types will be different hence separate type parameters
+    template <typename VoidLambda1, typename VoidLambda2>
+    auto inTransaction(VoidLambda1 t, VoidLambda2 c) const
+    {
+      [CATransaction begin];
+      [CATransaction setCompletionBlock:^{ c(); }];
+      t();
+      [CATransaction commit];
+    }
+  };
+
+  class ComponentAnimationsController final {
+  public:
+    ComponentAnimationsController(CKComponentAnimations animations)
+    : _animations(std::move(animations)),
+    _appliedAnimationsOnInitialMount(std::make_shared<AppliedAnimationsByComponentMap>()),
+    _appliedAnimationsFromPreviousComponent(std::make_shared<AppliedAnimationsByComponentMap>()) {};
+
+    void collectPendingAnimations();
+
+    template <typename TransactionProvider>
+    void applyPendingAnimations(TransactionProvider& transactionProvider)
+    {
+      applyPendingAnimations(_pendingAnimationsOnInitialMount,
+                             _appliedAnimationsOnInitialMount,
+                             transactionProvider);
+      applyPendingAnimations(_pendingAnimationsFromPreviousComponent,
+                             _appliedAnimationsFromPreviousComponent,
+                             transactionProvider);
+    }
+
+    void cleanupAppliedAnimationsForComponent(CKComponent *const c);
+
+  private:
+    using PendingAnimationsByComponentMap = std::unordered_map<CKComponent *, std::vector<CKPendingComponentAnimation>, CK::hash<CKComponent *>, CK::is_equal<CKComponent *>>;
+    using AppliedAnimationsByComponentMap = std::unordered_map<CKComponent *, CKAppliedComponentAnimationMap, CK::hash<CKComponent *>, CK::is_equal<CKComponent *>>;
+
+    template <typename TransactionProvider>
+    auto applyPendingAnimations(const PendingAnimationsByComponentMap &pendingAnimations,
+                                const std::shared_ptr<AppliedAnimationsByComponentMap> &appliedAnimations,
+                                TransactionProvider& transactionProvider)
+    {
+      for (const auto &kv : pendingAnimations) {
+        for (const auto &pa : kv.second) {
+          applyPendingAnimationForComponent(appliedAnimations,
+                                            pa,
+                                            kv.first,
+                                            transactionProvider);
+        }
+      }
+    }
+
+    template <typename TransactionProvider>
+    auto applyPendingAnimationForComponent(const std::shared_ptr<AppliedAnimationsByComponentMap> &animations,
+                                           const CKPendingComponentAnimation &pa,
+                                           CKComponent *const c,
+                                           TransactionProvider& transactionProvider)
+    {
+      const auto animationID = _animationID++;
+      transactionProvider.inTransaction([pa, c, animationID, animations](){
+        const auto animation = pa.animation;
+        auto &animationsForComponent = (*animations)[c];
+        const auto appliedAnimation = CKAppliedComponentAnimation {animation, animation.didRemount(pa.context)};
+        animationsForComponent.insert({animationID, appliedAnimation});
+      }, [pa, c, animationID, animations](){
+        auto &animationsForComponent = (*animations)[c];
+        const auto it = animationsForComponent.find(animationID);
+        if (it == animationsForComponent.end()) { return; }
+        pa.animation.cleanup(it->second.context);
+        animationsForComponent.erase(it);
+      });
+    }
+
+    static auto cleanupAppliedAnimationsForComponent(AppliedAnimationsByComponentMap &aas,
+                                                     CKComponent *const c);
+
+    const CKComponentAnimations _animations;
+    PendingAnimationsByComponentMap _pendingAnimationsOnInitialMount = {};
+    PendingAnimationsByComponentMap _pendingAnimationsFromPreviousComponent = {};
+    // Ownership will be shared with transaction completions which can outlive the controller
+    std::shared_ptr<AppliedAnimationsByComponentMap> _appliedAnimationsOnInitialMount;
+    std::shared_ptr<AppliedAnimationsByComponentMap> _appliedAnimationsFromPreviousComponent;
+    int _animationID = 0;
+  };
+}
---
+++ ComponentKit/Core/CKComponentAnimationsController.mm
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import "CKComponentAnimationsController.h"
+
+#import <ComponentKit/CKAssert.h>
+#import <ComponentKit/ComponentUtilities.h>
+
+namespace CK {
+  static auto makePendingAnimation(const CKComponentAnimation &a)
+  {
+    return CKPendingComponentAnimation {a, a.willRemount()};
+  }
+
+  void ComponentAnimationsController::collectPendingAnimations()
+  {
+    CKCAssert(_appliedAnimationsOnInitialMount->empty(),
+              @"Instances of CK::ComponentAnimationsController can't be reused.");
+
+    std::transform(_animations.animationsOnInitialMount().begin(),
+                   _animations.animationsOnInitialMount().end(),
+                   std::inserter(_pendingAnimationsOnInitialMount, _pendingAnimationsOnInitialMount.begin()),
+                   [](const auto &kv){ return std::make_pair(kv.first, map(kv.second, makePendingAnimation)); });
+
+    std::transform(_animations.animationsFromPreviousComponent().begin(),
+                   _animations.animationsFromPreviousComponent().end(),
+                   std::inserter(_pendingAnimationsFromPreviousComponent, _pendingAnimationsFromPreviousComponent.begin()),
+                   [](const auto &kv){ return std::make_pair(kv.first, map(kv.second, makePendingAnimation)); });
+  }
+
+  auto ComponentAnimationsController::cleanupAppliedAnimationsForComponent(AppliedAnimationsByComponentMap &aas, CKComponent *const c)
+  {
+    for (const auto &kv : aas[c]) {
+      const auto a = kv.second;
+      a.animation.cleanup(a.context);
+    }
+    aas.erase(c);
+  }
+
+  void ComponentAnimationsController::cleanupAppliedAnimationsForComponent(CKComponent *const c)
+  {
+    cleanupAppliedAnimationsForComponent(*_appliedAnimationsOnInitialMount, c);
+    cleanupAppliedAnimationsForComponent(*_appliedAnimationsFromPreviousComponent, c);
+  }
+}
