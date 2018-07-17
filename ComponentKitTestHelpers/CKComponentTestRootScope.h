5455bad747bf800499980163309928c59316e7c6
Eric Czarny <eczarny@fb.com>
2018-07-17 13:35:22 -0700

    Component test root scopes should not be created if a previous thread local component scope exists
    Summary: `CKComponentTestRootScope` should only be used in tests. To help avoid issues where folks may accidentally use a component test root scope outside of a test I added an assert.
    
    Reviewed By: Andrey-Mishanin
    
    Differential Revision: D8876331
    
    fbshipit-source-id: 6a02d336338abb90f269a6336e20526870a1acb0

--- ComponentKitTestHelpers/CKComponentTestRootScope.h
+++ ComponentKitTestHelpers/CKComponentTestRootScope.h
@@ -20,11 +20,25 @@
    CKComponentTestRootScope scope;
    CKComponent *c = ...;
 
- In the example above the test root scope will be made available to the component and all of its children.
+ In the example above the component test root scope will be made available to the component and all of its children.
  */
 class CKComponentTestRootScope {
-public:
-  CKComponentTestRootScope() : _threadScope(CKComponentScopeRootWithDefaultPredicates(nil, nil), {}) {};
-private:
-  CKThreadLocalComponentScope _threadScope;
+ public:
+  CKComponentTestRootScope()
+      : _previousThreadLocalComponentScope(CKThreadLocalComponentScope::currentScope()),
+        _threadLocalComponentScope(CKComponentScopeRootWithDefaultPredicates(nil, nil), {}) {
+    /*
+     The component test root scope must be created outside of any existing component scope.
+     If a previous thread local component scope exists then odds are good the component test root scope is being used
+     outside of a test.FBSwitchComponentServerSnapshotTests.mm Component test root scopes are intended to only be used by tests, and attempting to use them
+     in production code can lead to subtle issues (e.g. dropping component state updates).
+     */
+     CKCAssert(_previousThreadLocalComponentScope == nullptr,
+               @"Unable to create a component test root scope if another component scope is available\n" \
+               "This can happen if a component test root scope is created outside of a test, this is not supported");
+  };
+
+ private:
+  CKThreadLocalComponentScope _threadLocalComponentScope;
+  CKThreadLocalComponentScope *_previousThreadLocalComponentScope;
 };
