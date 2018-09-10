3adc712237187807273f7bf8922405761c00124f
Oleksii Dykan <alickbass@fb.com>
2018-09-10 05:23:56 -0700

    Add CKRequired type to ComponentKit
    Summary: This diff adds a CKRequired type that is used in the structs to make sure that there is always a value provided.
    
    Reviewed By: adamjernst
    
    Differential Revision: D9702512
    
    fbshipit-source-id: 716b4722b0f2597349d455e753dcd95a4dafc17e

---
+++ ComponentKit/Utilities/CKRequired.h
@@ -0,0 +1,41 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include <utility>
+
+/**
+ CKRequired<T> uses the compiler to enforce definition of a struct member (primitives, pointers, or objects).
+
+ Internally, we use an implicit constructor without a default, so there has to be an initial value.
+
+ Usage:
+ @code
+ struct S {
+ CKRequired<int> i;
+ CKRequired<NSString *> str;
+ NSString *optionalStr;
+ };
+
+ S options = {
+ .i = 0,                // warning if omitted
+ .str = @"Hello World", // warning if omitted
+ };
+ @endcode
+ */
+template <typename T>
+struct CKRequired {
+  /// Pass-through constructor (allows for implicit conversion) for wrapped type T
+  template<typename... Args>
+  CKRequired(Args&&... args): _t(std::forward<Args>(args)...) {
+    static_assert(sizeof...(Args) > 0, "Required struct member not initialized. Expand assert trace to see where this was triggered.");
+  }
+
+  /// Public accessor for private storage (Use when implicit conversion is impracticable)
+  T get() const { return _t; }
+
+  // Implicit conversion
+  operator T() const { return _t; }
+  operator T&() { return _t; }
+
+private:
+  T _t;
+};
