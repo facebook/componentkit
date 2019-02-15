2055e5e93fd0a7a80a28e0417346b581e2449b2e
Kfir Schindelhaim <kfirs@fb.com>
2019-02-15 01:58:07 -0800

    Report the correct node in didReuseNode
    Summary:
    Report the correct reused node from `CKRenderHelpers`.
    Even though we reuse the render's child component and not the component itself, we should still report back the node of the render component. Otherwise, we get random components in the didReuseNode annotations instead of the relevant render components.
    
    Reviewed By: fabiomassimo
    
    Differential Revision: D14099461
    
    fbshipit-source-id: 45321dcf660437a70a70801977e6d046cde87553

--- ComponentKit/Utilities/CKRenderHelpers.mm
+++ ComponentKit/Utilities/CKRenderHelpers.mm
@@ -49,7 +49,7 @@ namespace CKRenderInternal {
     [component didReuseComponent:(id<CKRenderComponentProtocol>)previousNode.component];
 
     // Notify scope root listener
-    [params.scopeRoot.analyticsListener didReuseNode:reusedChild inScopeRoot:params.scopeRoot fromPreviousScopeRoot:params.previousScopeRoot];
+    [params.scopeRoot.analyticsListener didReuseNode:node inScopeRoot:params.scopeRoot fromPreviousScopeRoot:params.previousScopeRoot];
   }
 
   // Reuse the previous component generation and its component tree and notify the previous component about it.
