b2007c9ca53a010fcf8e5276a756ff1ae21b656b
Kevin Lin <kevinlyn@fb.com>
2019-04-12 13:20:21 -0700

    Added helper function for getting removed component controllers between scope roots
    Summary: Added helper function for getting removed component controllers between scope roots.
    
    Reviewed By: kfirapps
    
    Differential Revision: D14859241
    
    fbshipit-source-id: 57b53795717823352ae7fcb81c17934a5abbf3f9

---
+++ ComponentKit/Core/CKComponentControllerHelper.h
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+
+#import <vector>
+
+#import <ComponentKit/CKComponentScopeTypes.h>
+
+@class CKComponentController;
+@class CKComponentScopeRoot;
+
+namespace CKComponentControllerHelper {
+  /**
+   Return component controllers, which match the predicate, that are not presented in the new scope root.
+   */
+  auto removedControllersFromPreviousScopeRootMatchingPredicate(CKComponentScopeRoot *newRoot,
+                                                                CKComponentScopeRoot *previousRoot,
+                                                                CKComponentControllerPredicate predicate) -> std::vector<CKComponentController *>;
+};
---
+++ ComponentKit/Core/CKComponentControllerHelper.mm
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import "CKComponentControllerHelper.h"
+
+#import <ComponentKit/CKComponentScopeRoot.h>
+
+namespace CKComponentControllerHelper {
+  auto removedControllersFromPreviousScopeRootMatchingPredicate(CKComponentScopeRoot *newRoot,
+                                                                CKComponentScopeRoot *previousRoot,
+                                                                CKComponentControllerPredicate predicate) -> std::vector<CKComponentController *>
+  {
+    if (!previousRoot) {
+      return {};
+    }
+    const auto oldControllers = [previousRoot componentControllersMatchingPredicate:predicate];
+    const auto newControllers = [newRoot componentControllersMatchingPredicate:predicate];
+    const auto removedControllers = CK::Collection::difference(oldControllers,
+                                                               newControllers,
+                                                               [](const auto &lhs, const auto &rhs){
+                                                                 return lhs == rhs;
+                                                               });
+    return CK::map(removedControllers, [](const auto controller){ return (CKComponentController *)controller; });
+  }
+};
