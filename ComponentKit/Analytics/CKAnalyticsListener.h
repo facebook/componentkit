17af869daafd261078f646ffd08821e3b3cb76b1
Kfir Schindelhaim <kfirs@fb.com>
2019-07-26 07:40:38 -0700

    Extract CKSystraceListener into a separate file
    Summary: Self-explanatory
    
    Reviewed By: alickbass
    
    Differential Revision: D16515901
    
    fbshipit-source-id: 0d7d71dfa2109b0f533f03670c961f4e39d4954b

--- ComponentKit/Analytics/CKAnalyticsListener.h
+++ ComponentKit/Analytics/CKAnalyticsListener.h
@@ -14,65 +14,12 @@
 #import <ComponentKit/ComponentMountContext.h>
 #import <ComponentKit/CKOptional.h>
 #import <ComponentKit/CKTreeNodeProtocol.h>
+#import <ComponentKit/CKSystraceListener.h>
 
 @protocol CKTreeNodeProtocol;
 
 @class CKComponent;
 @class CKComponentScopeRoot;
-
-/**
- This protocol is being used by the infrastructure to collect data of the components' life cycles.
-
- Will be used only when systrace is enabled.
- */
-@protocol CKSystraceListener <NSObject>
-
-/**
- Start/End a block trace in systrace.
- */
-- (void)willStartBlockTrace:(const char *const)blockName;
-- (void)didStartBlockTrace:(const char *const)blockName;
-
-/**
- Called before/after building a scoped component.
-
- Will be called only when systrace is enabled.
- */
-- (void)willBuildComponent:(Class)componentClass;
-- (void)didBuildComponent:(Class)componentClass;
-
-/**
- Called before/after mounting a component.
-
- Will be called only when systrace is enabled.
- */
-- (void)willMountComponent:(CKComponent *)component;
-- (void)didMountComponent:(CKComponent *)component;
-
-/**
- Called before/after layout a component.
-
- Will be called only when systrace is enabled.
- */
-- (void)willLayoutComponent:(CKComponent *)component;
-- (void)didLayoutComponent:(CKComponent *)component;
-
-/**
-  Called before/after evaluating a component should be updated or not.
-
-  Will be called only when systrace is enabled.
-*/
-- (void)willCheckShouldComponentUpdate:(CKComponent *)component;
-- (void)didCheckShouldComponentUpdate:(CKComponent *)component;
-
-/**
- Called before/after we block on the main thread because we're not yet finished
- generating the component on a background thread.
- */
-- (void)willBlockThreadOnGeneratingItemLayout;
-- (void)didBlockThreadOnGeneratingItemLayout;
-
-@end
 
 @protocol CKDebugAnalyticsListener <NSObject>
 
---
+++ ComponentKit/Analytics/CKSystraceListener.h
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+#import <ComponentKit/CKComponent.h>
+
+/**
+ This protocol is being used by the infrastructure to collect data into systrace if enabled.
+ */
+@protocol CKSystraceListener <NSObject>
+
+/**
+ Start/End a block trace in systrace.
+ */
+- (void)willStartBlockTrace:(const char *const)blockName;
+- (void)didStartBlockTrace:(const char *const)blockName;
+
+/**
+ Called before/after building a scoped component.
+
+ Will be called only when systrace is enabled.
+ */
+- (void)willBuildComponent:(Class)componentClass;
+- (void)didBuildComponent:(Class)componentClass;
+
+/**
+ Called before/after mounting a component.
+
+ Will be called only when systrace is enabled.
+ */
+- (void)willMountComponent:(CKComponent *)component;
+- (void)didMountComponent:(CKComponent *)component;
+
+/**
+ Called before/after layout a component.
+
+ Will be called only when systrace is enabled.
+ */
+- (void)willLayoutComponent:(CKComponent *)component;
+- (void)didLayoutComponent:(CKComponent *)component;
+
+/**
+  Called before/after evaluating a component should be updated or not.
+
+  Will be called only when systrace is enabled.
+*/
+- (void)willCheckShouldComponentUpdate:(CKComponent *)component;
+- (void)didCheckShouldComponentUpdate:(CKComponent *)component;
+
+/**
+ Called before/after we block on the main thread because we're not yet finished
+ generating the component on a background thread.
+ */
+- (void)willBlockThreadOnGeneratingItemLayout;
+- (void)didBlockThreadOnGeneratingItemLayout;
+
+@end
