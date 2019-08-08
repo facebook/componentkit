d78d8200c8364203d38e9167509a973bdcfc87a7
Andrey Mishanin <amishanin@fb.com>
2019-08-08 13:44:22 -0700

    Non-null pointer type
    Summary:
    `CK::RelaxedNonNull<Ptr>` has all the features described below but allows *implicit* construction from a nullable pointer. This type is only supposed to be used in client-facing APIs such as `updateState:mode:` or `CKComponentAnimation` so client code doesn’t get littered with calls to `CK::makeNonNull`.
    
    The second type, `CK::NonNull` inherits from `CK::RelaxedNonNull` but only allows *explicit* construction from a nullable pointer. This is the type that should be used for the internal APIs of the framework.
    
    ### Features
    
    * No default constructor
    * Call ObjC methods as usual
    * Access ivars as usual
    * Conversion back to nullable
    * Comparison to nullable
    
    ### Compile-time Checks
    
    * No construction or assignment from `nil`
    * No comparison to `nil`
    * No conversion to `bool`
    
    Reviewed By: kfirapps
    
    Differential Revision: D16620674
    
    fbshipit-source-id: 7f41518c8c9f984b7656c2ddb416b6fb465dc80a

---
+++ ComponentKit/Base/CKNonNull.h
@@ -0,0 +1,101 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#import <ComponentKit/CKAssert.h>
+
+namespace CK {
+
+template <typename> class RelaxedNonNull;
+template <typename> class NonNull;
+
+namespace NonNullDetail {
+template <typename> struct IsNonNull: public std::false_type {};
+template <typename Ptr> struct IsNonNull<RelaxedNonNull<Ptr>>: public std::true_type {};
+template <typename Ptr> struct IsNonNull<NonNull<Ptr>>: public std::true_type {};
+} // NonNullDetail
+
+template <typename Ptr>
+class RelaxedNonNull {
+public:
+  static_assert(!NonNullDetail::IsNonNull<Ptr>::value, "Pointer is already non-null");
+
+  RelaxedNonNull(const Ptr &ptr) :_ptr(ptr) { CKCAssertNotNil(_ptr, @"The pointer can't be nil"); }
+  RelaxedNonNull(Ptr &&ptr) :_ptr(ptr) { CKCAssertNotNil(_ptr, @"The pointer can't be nil"); }
+
+  template <typename OtherPtr, typename = std::enable_if_t<std::is_convertible<OtherPtr, Ptr>::value>>
+  RelaxedNonNull(const RelaxedNonNull<OtherPtr> &ptr) :_ptr(ptr.operator OtherPtr()) {}
+
+  // Disallow assignment from nullable
+  auto operator =(const Ptr &) -> RelaxedNonNull & = delete;
+
+  // Implicit conversion to nullable
+  operator Ptr () const & { return _ptr; }
+  operator Ptr &&() && { return std::move(_ptr); }
+
+  // Passthrough
+  Ptr operator ->() const { return _ptr; }
+  Ptr operator *() const { return _ptr; }
+
+  template <typename... Args>
+  auto operator ()(Args &&... args) const { return _ptr(std::forward<Args>(args)...); }
+
+  // Disallow conversion to bool
+  operator bool () const = delete;
+
+  // Disallow nil literals
+  RelaxedNonNull(std::nullptr_t) = delete;
+
+  template <typename L, typename R>
+  friend auto operator ==(const RelaxedNonNull<L> &, const RelaxedNonNull<R> &) -> bool;
+
+private:
+  Ptr _ptr;
+};
+
+template <typename L, typename R>
+auto operator ==(const RelaxedNonNull<L> &lhs, const RelaxedNonNull<R> &rhs) -> bool
+{
+  return lhs._ptr == rhs._ptr;
+}
+
+template <typename L, typename R>
+auto operator !=(const RelaxedNonNull<L> &lhs, const RelaxedNonNull<R> &rhs) -> bool
+{
+  return !(lhs == rhs);
+}
+
+template <typename Ptr>
+auto operator ==(const RelaxedNonNull<Ptr> &lhs, std::nullptr_t rhs) -> bool = delete;
+template <typename Ptr>
+auto operator !=(const RelaxedNonNull<Ptr> &lhs, std::nullptr_t rhs) -> bool = delete;
+
+template <typename Ptr>
+class NonNull final: public RelaxedNonNull<Ptr> {
+public:
+  // Explicit construction from nullable
+  explicit NonNull(const Ptr &ptr) : RelaxedNonNull<Ptr>{ptr} {}
+  explicit NonNull(Ptr &&ptr) : RelaxedNonNull<Ptr>{std::move(ptr)} {}
+
+  template <typename OtherPtr, typename = std::enable_if_t<std::is_convertible<OtherPtr, Ptr>::value>>
+  NonNull(const NonNull<OtherPtr> &ptr) : RelaxedNonNull<Ptr>{ptr} {}
+
+  template <typename OtherPtr, typename = std::enable_if_t<std::is_convertible<OtherPtr, Ptr>::value>>
+  NonNull(const RelaxedNonNull<OtherPtr> &ptr) : RelaxedNonNull<Ptr>{ptr} {}
+
+  // Disallow nil literals
+  NonNull(std::nullptr_t) = delete;
+};
+
+template <typename Ptr>
+auto makeNonNull(Ptr p) { return NonNull<Ptr>{std::move(p)}; }
+
+} // namespace CK
---
+++ ComponentKitTests/CKNonNullTests.mm
@@ -0,0 +1,85 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import <XCTest/XCTest.h>
+
+#import <ComponentKit/CKNonNull.h>
+
+using namespace CK;
+
+@interface CKRelaxedNonNullPtrTests : XCTestCase
+@end
+
+@implementation CKRelaxedNonNullPtrTests
+
+- (void)test_ImplicitConversionFromNullable
+{
+  auto const nullableSet = [NSSet set];
+  RelaxedNonNull<NSSet *> nonNullSet = nullableSet;
+
+  XCTAssertEqual(nonNullSet, nullableSet);
+}
+
+- (void)compileTimeChecks
+{
+  auto nn = RelaxedNonNull<NSObject *>([NSObject new]);
+
+//  if (nn) {}
+//  auto nn1 = RelaxedNonNull<NSObject *>{nil};
+//  nn = nil;
+//  if (nn == nil) {}
+//  if (nn != nil) {}
+//  auto nn2 = RelaxedNonNull<RelaxedNonNull<NSObject *>>{nn};
+}
+
+@end
+
+@interface CKNonNullPtrTests : XCTestCase
+@end
+
+@implementation CKNonNullPtrTests
+
+- (void)test_ImplicitConversionToNullable
+{
+  auto const o = [NSObject new];
+  auto const nn = makeNonNull(o);
+
+  XCTAssertEqual([nn isProxy], [o isProxy]);
+}
+
+- (void)test_ConversionConstructorFromDerivedPtrType
+{
+  auto const ms = makeNonNull([NSMutableSet new]);
+  NonNull<NSSet *> s = ms;
+
+  XCTAssertEqualObjects(ms, s);
+}
+
+- (void)test_ImplicitConversionToNullablePtrToBase
+{
+  auto const nnms = makeNonNull([NSMutableSet new]);
+  NSSet *set = nnms;
+
+  XCTAssertEqualObjects(set, nnms);
+}
+
+- (void)compileTimeChecks
+{
+  auto nn = makeNonNull([NSObject new]);
+
+//  if (nn) {}
+//  auto nn1 = NonNull<NSObject *>{nil};
+//  nn = nil;
+//  if (nn == nil) {}
+//  if (nn != nil) {}
+//  auto nn2 = makeNonNull(nn);
+}
+
+@end
