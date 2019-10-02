28f7a26904e188df91d4f9a490564f08fbebd3c5
Kevin Lin <kevinlyn@fb.com>
2019-10-02 05:49:23 -0700

    `CKComponentGenerator`
    Summary:
    `CKComponentGenerator` is responsibile for maintaining scope root, generating component and listening to component state update.
    It exposes methods to generate component synchronously and asynchronously.
    
    Reviewed By: kfirapps
    
    Differential Revision: D17684949
    
    fbshipit-source-id: 5bf90d19827b30bd5a5d5f6412c7e17c17f536b6

---
+++ ComponentKit/Core/CKComponentGenerator.h
@@ -0,0 +1,132 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import <CoreGraphics/CoreGraphics.h>
+#import <Foundation/Foundation.h>
+
+#import <unordered_set>
+
+#import <ComponentKit/CKBuildComponentResult.h>
+#import <ComponentKit/CKComponentProvider.h>
+#import <ComponentKit/CKComponentScopeTypes.h>
+#import <ComponentKit/CKMacros.h>
+#import <ComponentKit/CKNonNull.h>
+#import <ComponentKit/CKUpdateMode.h>
+
+@class CKComponentGenerator;
+@class CKComponentScopeRoot;
+@protocol CKAnalyticsListener;
+
+@protocol CKComponentGeneratorDelegate <NSObject>
+
+/**
+ This is called on the affined queue before applying result from asynchronous generation.
+ It gives a chance to discard the result by returning `NO`.
+ */
+- (BOOL)componentGeneratorShouldApplyAsynchronousGenerationResult:(CKComponentGenerator *)componentGenerator;
+
+/**
+ This is called on the affined queue when asynchronous component generation is finished.
+ */
+- (void)componentGenerator:(CKComponentGenerator *)componentGenerator didAsynchronouslyGenerateComponentResult:(CKBuildComponentResult)result;
+
+/**
+ This is called on the affined queue when a component state update is received.
+ You should normally call either `generateComponentSynchronously` or `generateComponentAsynchronously` after this delegate method is called.
+ */
+- (void)componentGenerator:(CKComponentGenerator *)componentGenerator didReceiveComponentStateUpdateWithMode:(CKUpdateMode)mode;
+
+@end
+
+struct CKComponentGeneratorOptions {
+  /**
+   A required option that makes sure callsite reacts to important events from `CKComponentGenerator`.
+   */
+  CK::NonNull<id<CKComponentGeneratorDelegate>> delegate;
+
+  /**
+   A `componentProvider` block that is called when generate component.
+   */
+  CK::NonNull<CKComponentProviderBlock> componentProvider;
+
+  /**
+   Optional `componentPredicates` that is used for creating `CKComponentScopeRoot`.
+   @see `CKComponentPredicate`
+   */
+  std::unordered_set<CKComponentPredicate> componentPredicates = {};
+
+  /**
+   Optional `componentControllerPredicates` that is used for creating `CKComponentScopeRoot`.
+   @see `CKComponentControllerPredicate`
+   */
+  std::unordered_set<CKComponentControllerPredicate> componentControllerPredicates = {};
+
+  /**
+   Specify if you would like to override the default `CKAnalyticsListener`.
+   */
+  id<CKAnalyticsListener> analyticsListener = nil;
+
+  /**
+   A queue that is used to perform all majors tasks in `CKComponentGenerator`.
+   All public APIs of `CKComponentGenerator` must be called on this queue.
+   Defaults to main queue.
+   */
+  dispatch_queue_t affinedQueue = dispatch_get_main_queue();
+};
+
+/**
+ `CKComponentGenerator` is responsibile for maintaining scope root, generating component and listening to component state update.
+ It exposes methods to generate component synchronously and asynchronously.
+ All APIs should be called on the `affinedQueue` that is passed in from `CKComponentGeneratorOptions`.
+ */
+@interface CKComponentGenerator : NSObject
+
+- (instancetype)initWithOptions:(const CKComponentGeneratorOptions &)options NS_DESIGNATED_INITIALIZER;
+- (instancetype)init CK_NOT_DESIGNATED_INITIALIZER_ATTRIBUTE;
+
+/**
+ Updates the model used to render the component.
+ */
+- (void)updateModel:(id<NSObject>)model;
+
+/**
+ Updates the context used to render the component.
+ */
+- (void)updateContext:(id<NSObject>)context;
+
+/**
+ Generate component synchronously on affined queue and return the result.
+ */
+- (CKBuildComponentResult)generateComponentSynchronously;
+
+/**
+ Generate component asynchronously on a global background queue.
+ `CKComponentGeneratorDelegate.componentGenerator:didAsynchronouslyGenerateComponentResult:` will be called once it
+ finishes generation.
+ */
+- (void)generateComponentAsynchronously;
+
+/**
+ Ignore component reuse in next component generation.
+ This should be used if you are going to update `CKComponentContext` in the hierarchy.
+ */
+- (void)ignoreComponentReuseInNextGeneration;
+
+/**
+ The underlying scope root that is maintained by `CKComponentGenerator`.
+ */
+- (CKComponentScopeRoot *)scopeRoot;
+
+/**
+ This is a temporary API for code migration. DO NOT USE.
+ */
+- (void)setScopeRoot:(CKComponentScopeRoot *)scopeRoot;
+
+@end
---
+++ ComponentKit/Core/CKComponentGenerator.mm
@@ -0,0 +1,223 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import "CKComponentGenerator.h"
+
+#import <ComponentKit/CKAssert.h>
+#import <ComponentKit/CKBuildComponent.h>
+#import <ComponentKit/CKComponentController.h>
+#import <ComponentKit/CKComponentControllerEvents.h>
+#import <ComponentKit/CKComponentControllerHelper.h>
+#import <ComponentKit/CKComponentScopeRoot.h>
+#import <ComponentKit/CKComponentScopeRootFactory.h>
+#import <ComponentKit/CKGlobalConfig.h>
+
+static void *kAffinedQueueKey = &kAffinedQueueKey;
+
+#define CKAssertAffinedQueue() CKAssert([self _isRunningOnAffinedQueue], @"This method must only be called on the affined queue")
+
+struct CKComponentGeneratorInputs {
+  CKComponentScopeRoot *scopeRoot;
+  id<NSObject> model;
+  id<NSObject> context;
+  CKComponentStateUpdateMap stateUpdates;
+  BOOL ignoreComponentReuse;
+
+  bool operator==(const CKComponentGeneratorInputs &i) const {
+    return scopeRoot == i.scopeRoot && model == i.model && context == i.context && stateUpdates == i.stateUpdates && ignoreComponentReuse == i.ignoreComponentReuse;
+  };
+};
+
+@interface CKComponentGenerator () <CKComponentStateListener>
+
+@end
+
+@implementation CKComponentGenerator
+{
+  CKComponentProviderBlock _componentProvider;
+  CKComponentGeneratorInputs _pendingInputs;
+  __weak id<CKComponentGeneratorDelegate> _delegate;
+  dispatch_queue_t _affinedQueue;
+}
+
+- (instancetype)initWithOptions:(const CKComponentGeneratorOptions &)options
+{
+  if (self = [super init]) {
+    _delegate = options.delegate;
+    _componentProvider = options.componentProvider;
+    _pendingInputs = {
+      .scopeRoot =
+      CKComponentScopeRootWithPredicates(self,
+                                         options.analyticsListener ?: CKReadGlobalConfig().defaultAnalyticsListener,
+                                         options.componentPredicates,
+                                         options.componentControllerPredicates)
+    };
+    _affinedQueue = options.affinedQueue;
+    if (_affinedQueue != dispatch_get_main_queue()) {
+      dispatch_queue_set_specific(_affinedQueue, kAffinedQueueKey, kAffinedQueueKey, NULL);
+    }
+  }
+  return self;
+}
+
+- (void)dealloc
+{
+  const auto scopeRoot = _pendingInputs.scopeRoot;
+  const auto invalidateController = ^{
+    CKComponentScopeRootAnnounceControllerInvalidation(scopeRoot);
+  };
+  if ([NSThread isMainThread]) {
+    invalidateController();
+  } else {
+    dispatch_async(dispatch_get_main_queue(), invalidateController);
+  }
+}
+
+- (void)updateModel:(id<NSObject>)model
+{
+  CKAssertAffinedQueue();
+  _pendingInputs.model = model;
+}
+
+- (void)updateContext:(id<NSObject>)context
+{
+  CKAssertAffinedQueue();
+  _pendingInputs.context = context;
+}
+
+- (CKBuildComponentResult)generateComponentSynchronously
+{
+  CKAssertAffinedQueue();
+
+  const auto ignoreComponentReuse = _pendingInputs.ignoreComponentReuse;
+  _pendingInputs.ignoreComponentReuse = NO;
+  const auto result = CKBuildComponent(_pendingInputs.scopeRoot, _pendingInputs.stateUpdates, ^{
+    return _componentProvider(_pendingInputs.model, _pendingInputs.context);
+  }, ignoreComponentReuse);
+  [self _applyResult:result invalidComponentControllers:_invalidComponentControllersBetweenScopeRoots(result.scopeRoot, _pendingInputs.scopeRoot)];
+  return result;
+}
+
+- (void)generateComponentAsynchronously
+{
+  CKAssertAffinedQueue();
+
+  const auto inputs = std::make_shared<const CKComponentGeneratorInputs>(_pendingInputs);
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    const auto result =
+    std::make_shared<const CKBuildComponentResult>(CKBuildComponent(
+                                                                    inputs->scopeRoot,
+                                                                    inputs->stateUpdates,
+                                                                    ^{
+                                                                      return _componentProvider(inputs->model, inputs->context);
+                                                                    },
+                                                                    inputs->ignoreComponentReuse));
+    const auto invalidComponentControllers =
+    std::make_shared<const std::vector<CKComponentController *>>(_invalidComponentControllersBetweenScopeRoots(result->scopeRoot, inputs->scopeRoot));
+    dispatch_async(_affinedQueue, ^{
+      if (![_delegate componentGeneratorShouldApplyAsynchronousGenerationResult:self]) {
+        return;
+      }
+      // If the inputs haven't changed, apply the result; otherwise, retry.
+      if (_pendingInputs == *inputs) {
+        _pendingInputs.ignoreComponentReuse = NO;
+        const auto componentControllers = invalidComponentControllers != nullptr ? *invalidComponentControllers : std::vector<CKComponentController *> {};
+
+        [self _applyResult:*result invalidComponentControllers:componentControllers];
+        [_delegate componentGenerator:self didAsynchronouslyGenerateComponentResult:*result];
+      } else {
+        [self generateComponentAsynchronously];
+      }
+    });
+  });
+}
+
+- (void)ignoreComponentReuseInNextGeneration
+{
+  CKAssertAffinedQueue();
+  _pendingInputs.ignoreComponentReuse = YES;
+}
+
+- (CKComponentScopeRoot *)scopeRoot
+{
+  CKAssertAffinedQueue();
+  return _pendingInputs.scopeRoot;
+}
+
+- (void)setScopeRoot:(CKComponentScopeRoot *)scopeRoot
+{
+  CKAssertAffinedQueue();
+  _pendingInputs.scopeRoot = scopeRoot;
+}
+
+#pragma mark - Private
+
+- (void)_applyResult:(const CKBuildComponentResult &)result invalidComponentControllers:(const std::vector<CKComponentController *> &)invalidComponentControllers
+{
+  CKAssertAffinedQueue();
+
+  const auto componentControllers = std::make_shared<std::vector<CKComponentController *>>(invalidComponentControllers);
+  const auto invalidateController = ^{
+    for (const auto componentController : *componentControllers) {
+      [componentController invalidateController];
+    }
+  };
+  if ([NSThread isMainThread]) {
+    invalidateController();
+  } else {
+    dispatch_async(dispatch_get_main_queue(), invalidateController);
+  }
+  _pendingInputs.scopeRoot = result.scopeRoot;
+  _pendingInputs.stateUpdates = {};
+}
+
+- (BOOL)_isRunningOnAffinedQueue
+{
+  if (_affinedQueue == dispatch_get_main_queue()) {
+    return [NSThread isMainThread];
+  } else {
+    return (dispatch_get_specific(kAffinedQueueKey) == kAffinedQueueKey);
+  }
+}
+
+static std::vector<CKComponentController *> _invalidComponentControllersBetweenScopeRoots(CKComponentScopeRoot *newRoot,
+                                                                                          CKComponentScopeRoot *previousRoot)
+{
+  if (!previousRoot) {
+    return {};
+  }
+  return
+  CKComponentControllerHelper::removedControllersFromPreviousScopeRootMatchingPredicate(newRoot,
+                                                                                        previousRoot,
+                                                                                        &CKComponentControllerInvalidateEventPredicate);
+}
+
+#pragma mark - CKComponentStateListener
+
+- (void)componentScopeHandle:(CKComponentScopeHandle *)handle
+              rootIdentifier:(CKComponentScopeRootIdentifier)rootIdentifier
+       didReceiveStateUpdate:(id (^)(id))stateUpdate
+                    metadata:(const CKStateUpdateMetadata)metadata
+                        mode:(CKUpdateMode)mode
+{
+  CKAssertMainThread();
+
+  const auto enqueueStateUpdate = ^{
+    _pendingInputs.stateUpdates[handle].push_back(stateUpdate);
+    [_delegate componentGenerator:self didReceiveComponentStateUpdateWithMode:mode];
+  };
+  if ([self _isRunningOnAffinedQueue]) {
+    enqueueStateUpdate();
+  } else {
+    dispatch_async(_affinedQueue, enqueueStateUpdate);
+  }
+}
+
+@end
