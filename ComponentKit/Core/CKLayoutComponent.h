2d71bcd58c96a4aadfac680b46b9b9efe97d05ed
Kfir Schindelhaim <kfirs@fb.com>
2019-12-27 15:05:44 -0800

    Add CKLayoutComponent
    Summary:
    New base component for components with a custom layout.
    
    This will replace `CKRenderLayoutComponent` and `CKRenderLayoutWithChildrenComponent`.
    
    Reviewed By: Andrey-Mishanin
    
    Differential Revision: D19193915
    
    fbshipit-source-id: 6c14a580db9635c3e46833d0c95992311c9daf61

---
+++ ComponentKit/Core/CKLayoutComponent.h
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import <UIKit/UIKit.h>
+
+#import <ComponentKit/CKComponent.h>
+
+/** This component should be used on non-leaf components with a custom layout . */
+@interface CKLayoutComponent : CKComponent
+
+@end
---
+++ ComponentKit/Core/CKLayoutComponent.mm
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import "CKLayoutComponent.h"
+
+#import "CKComponentInternal.h"
+#import "CKComponentSubclass.h"
+#import "CKRenderHelpers.h"
+
+@implementation CKLayoutComponent
+
+- (void)buildComponentTree:(id<CKTreeNodeWithChildrenProtocol>)parent
+            previousParent:(id<CKTreeNodeWithChildrenProtocol>)previousParent
+                    params:(const CKBuildComponentTreeParams &)params
+      parentHasStateUpdate:(BOOL)parentHasStateUpdate
+{
+  CKRender::ComponentTree::Iterable::build(self, parent, previousParent, params, parentHasStateUpdate);
+}
+
+#pragma mark - CKMountable
+
+- (unsigned int)numberOfChildren
+{
+  CKFailAssert(@"%@ MUST override the '%@' method.", [self class], NSStringFromSelector(_cmd));
+  return 0;
+}
+
+- (id<CKMountable>)childAtIndex:(unsigned int)index
+{
+  CKFailAssert(@"%@ MUST override the '%@' method.", [self class], NSStringFromSelector(_cmd));
+  return nil;
+}
+
+- (CKComponentLayout)computeLayoutThatFits:(CKSizeRange)constrainedSize
+{
+  CKFailAssert(@"%@ MUST override the '%@' method.", [self class], NSStringFromSelector(_cmd));
+  return [super computeLayoutThatFits:constrainedSize];
+}
+
+@end
