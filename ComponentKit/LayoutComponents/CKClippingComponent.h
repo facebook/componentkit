1e8ec811bd07ed1e87ec703357fc5db8729a1055
Andrey Mishanin <amishanin@fb.com>
2020-03-23 10:39:03 -0700

    Clipping component
    Summary: Component that can clip its child component instead of shrinking it i.e. when given a dimension(s) to clip the child component will be given an infinite amount of space in that dimension, but the clipping component itself will maintain the size it was initialised with.
    
    Reviewed By: kevin0571
    
    Differential Revision: D20587272
    
    fbshipit-source-id: a7e28c459a260bae27bb2b2b262e71c51e676c3d

---
+++ ComponentKit/LayoutComponents/CKClippingComponent.h
@@ -0,0 +1,101 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import <ComponentKit/CKDefines.h>
+
+#if CK_NOT_SWIFT
+
+#import <ComponentKit/ComponentBuilder.h>
+
+namespace CK {
+enum class ClippingComponentDimensions {
+  none,
+  width,
+  height
+};
+
+namespace BuilderDetails {
+namespace ClippingComponentPropId {
+constexpr static auto component = ComponentBuilderBaseSizeOnlyPropId::__max << 1;
+constexpr static auto clippedDimensions = component << 1;
+constexpr static auto __max = clippedDimensions;
+}
+
+namespace ClippingComponentDetails {
+auto factory(CKComponent *, const CKComponentSize &, ClippingComponentDimensions) -> CKComponent *;
+}
+
+template <PropsBitmapType PropsBitmap = 0>
+class __attribute__((__may_alias__)) ClippingComponentBuilder
+: public ComponentBuilderBaseSizeOnly<ClippingComponentBuilder, PropsBitmap> {
+public:
+  ClippingComponentBuilder() = default;
+
+  ~ClippingComponentBuilder() = default;
+
+  /**
+   The child component whose layout will be clipped to the specified size.
+   */
+  auto &component(NS_RELEASES_ARGUMENT CKComponent *c)
+  {
+    constexpr auto componentIsNotSet = !PropBitmap::isSet(PropsBitmap, ClippingComponentPropId::component);
+    static_assert(componentIsNotSet, "Property 'component' is already set.");
+    _component = c;
+    return reinterpret_cast<
+    ClippingComponentBuilder<PropsBitmap | ClippingComponentPropId::component> &>(*this);
+  }
+
+  /**
+   A value determining which dimension(s) of child component layout could potentially be clipped. When calculating the child layout, the corresponding dimension(s)
+   will be unconstrained and potentially clipped as a result.
+   */
+  auto &clippedDimensions(ClippingComponentDimensions d)
+  {
+    constexpr auto clippedDimenstionsAreNotSet = !PropBitmap::isSet(PropsBitmap, ClippingComponentPropId::clippedDimensions);
+    static_assert(clippedDimenstionsAreNotSet, "Property 'clippedDimensions' is already set.");
+    _dimensions = d;
+    return reinterpret_cast<
+    ClippingComponentBuilder<PropsBitmap | ClippingComponentPropId::clippedDimensions> &>(*this);
+  }
+
+private:
+  friend BuilderBase<ClippingComponentBuilder, PropsBitmap>;
+
+  /**
+   Creates a new component instance with specified properties.
+
+   @note  This method must @b not be called more than once on a given component builder instance.
+   */
+  NS_RETURNS_RETAINED auto _build() noexcept -> CKComponent *
+  {
+    constexpr auto componentIsSet = PropBitmap::isSet(PropsBitmap, ClippingComponentPropId::component);
+    static_assert(componentIsSet, "Required property 'component' is not set.");
+    constexpr auto clippedDimenstionsAreSet = PropBitmap::isSet(PropsBitmap, ClippingComponentPropId::clippedDimensions);
+    static_assert(clippedDimenstionsAreSet, "Required property 'clippedDimensions' is not set.");
+
+    return ClippingComponentDetails::factory(_component, this->_size, _dimensions);
+  }
+  
+private:
+  CKComponent *_component;
+  ClippingComponentDimensions _dimensions;
+};
+
+}
+
+/**
+ Component that can clip its child component instead of shrinking it i.e. when given a dimension(s) to clip the child 
+ component will be given an infinite amount of space in that dimension, but the clipping component itself will maintain 
+ the size it was initialised with.
+ */
+using ClippingComponentBuilder = BuilderDetails::ClippingComponentBuilder<>;
+}
+
+#endif
---
+++ ComponentKit/LayoutComponents/CKClippingComponent.mm
@@ -0,0 +1,98 @@
+#import <ComponentKit/CKClippingComponent.h>
+
+#import <ComponentKit/CKAssert.h>
+#import <ComponentKit/CKComponentSubclass.h>
+#import <ComponentKit/CKLayoutComponent.h>
+
+@interface CKClippingComponent : CKLayoutComponent
++ (instancetype)newWithComponent:(CKComponent *)child
+                            size:(const CKComponentSize &)size
+               clippedDimensions:(CK::ClippingComponentDimensions)dimensions;
+@end
+
+@implementation CKClippingComponent {
+  CKComponent *_component;
+  CK::ClippingComponentDimensions _clippedDimensions;
+}
+
++ (instancetype)newWithComponent:(CKComponent *)component
+                            size:(const CKComponentSize &)size
+               clippedDimensions:(CK::ClippingComponentDimensions)dimensions
+{
+  if (component == nil) {
+    return nil;
+  }
+
+  auto const c =
+  [super
+   newWithView:
+   CK::ViewConfig()
+   .viewClass([UIView class])
+   .clipsToBounds(true)
+   .build()
+   size:size];
+  c->_component = component;
+  c->_clippedDimensions = dimensions;
+  return c;
+}
+
+- (unsigned int)numberOfChildren
+{
+  return CKIterable::numberOfChildren(_component);
+}
+
+- (id<CKIterable>)childAtIndex:(unsigned int)index
+{
+  return CKIterable::childAtIndex(self, index, _component);
+}
+
+- (CKComponentLayout)computeLayoutThatFits:(CKSizeRange)constrainedSize
+                          restrictedToSize:(const CKComponentSize &)size
+                      relativeToParentSize:(CGSize)parentSize
+{
+  auto const resolvedRange = constrainedSize.intersect(size.resolve(parentSize));
+  
+  // Non-const to enable the move out of lambda.
+  auto childLayout = [_component layoutThatFits:resolvedRange parentSize:parentSize];
+
+  auto const finalLayout = [&](){
+    if (_clippedDimensions == CK::ClippingComponentDimensions::none) {
+      return childLayout;
+    }
+
+    auto const unconstrainedSize = CKSizeRange{
+      constrainedSize.min,
+      adjustedMaxSizeForClippedDimensions(constrainedSize.max, _clippedDimensions)
+    };
+    return [_component layoutThatFits:unconstrainedSize parentSize:parentSize];
+  }();
+  
+  return {
+    self,
+    // This component will always have the "normal" size as if the child *was* always constrained.
+    resolvedRange.clamp(childLayout.size),
+    std::vector<CKComponentLayoutChild> {
+      {CGPointZero, finalLayout}
+    }
+  };
+}
+
+static auto adjustedMaxSizeForClippedDimensions(CGSize originalMaxSize, CK::ClippingComponentDimensions clippedDimensions) -> CGSize
+{
+  switch (clippedDimensions) {
+    case CK::ClippingComponentDimensions::width:
+      return {INFINITY, originalMaxSize.height};
+    case CK::ClippingComponentDimensions::height:
+      return {originalMaxSize.width, INFINITY};
+    case CK::ClippingComponentDimensions::none:
+      CKCFailAssert(@"When no dimension is clipped, the original size constraints must be used");
+      return originalMaxSize;
+  }
+}
+
+@end
+
+auto CK::BuilderDetails::ClippingComponentDetails::factory(CKComponent *component, const CKComponentSize &size, CK::ClippingComponentDimensions dimensions) -> CKComponent *
+{
+  return [CKClippingComponent newWithComponent:component size:size clippedDimensions:dimensions];
+}
