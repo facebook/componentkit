e35fea711f006fdb685207630680a583207a140a
Kevin Lin <kevinlyn@fb.com>
2020-03-26 05:16:14 -0700

    `NSAssert` -> `CKAssert` in `CKStatefulViewComponentController`
    Summary: `NSAssert` -> `CKAssert` in `CKStatefulViewComponentController`.
    
    Reviewed By: cuva
    
    Differential Revision: D20668663
    
    fbshipit-source-id: a377e1d0f9ea296afa3cbc7e312ca69dc04f2ee8

--- ComponentKit/StatefulViews/CKStatefulViewComponentController.mm
+++ ComponentKit/StatefulViews/CKStatefulViewComponentController.mm
@@ -27,7 +27,7 @@
 
 + (UIView *)newStatefulView:(id)context
 {
-  NSAssert(false, @"Should be implemented by subclasses.");
+  CKFailAssertWithCategory(NSStringFromClass(self), @"Should be implemented by subclasses.");
   return nil;
 }
 
@@ -39,7 +39,7 @@
 + (void)configureStatefulView:(UIView *)statefulView
                  forComponent:(CKComponent *)component
 {
-  NSAssert(false, @"Should be implemented by subclasses.");
+  CKFailAssertWithCategory(NSStringFromClass(self), @"Should be implemented by subclasses.");
 }
 
 + (NSInteger)maximumPoolSize:(id)context
@@ -77,10 +77,11 @@
 {
   [super didMount];
 
-  NSAssert([[self component] isKindOfClass:[CKStatefulViewComponent class]], @"Component should be a stateful view component.");
-  NSAssert(
+  CKAssertWithCategory([[self component] isKindOfClass:[CKStatefulViewComponent class]], self.component.className, @"Component should be a stateful view component.");
+  CKAssertWithCategory(
     method_getImplementation(class_getInstanceMethod([CKStatefulViewComponentController class], @selector(statefulView))) ==
     method_getImplementation(class_getInstanceMethod([self class], @selector(statefulView))),
+    self.component.className,
     @"Should not override the method -statefulView.");
 
   if (_statefulView == nil) {
@@ -126,7 +127,7 @@
   const CKComponentViewContext &context = [[self component] viewContext];
   [_statefulView setFrame:context.frame];
 
-  NSAssert([context.view.subviews count] <= 1, @"Should never have more than a single stateful subview.");
+  CKAssertWithCategory([context.view.subviews count] <= 1, self.component.className, @"Should never have more than a single stateful subview.");
   UIView *existingView = [context.view.subviews lastObject];
   if (existingView != _statefulView) {
     [existingView removeFromSuperview];
