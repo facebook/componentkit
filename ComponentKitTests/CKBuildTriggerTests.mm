3ff1763567d901276cbc028a79dd42f254b6fbc6
Hugo Cuvillier <cuva@fb.com>
2020-09-16 04:54:32 -0700

    Add tests for `CKBuildComponentTrigger`
    Summary: Let's get this tested.
    
    Reviewed By: fabiomassimo
    
    Differential Revision: D23677673
    
    fbshipit-source-id: fbdbd5f3b242079d0614619d4fa355cf7eccc9d1

---
+++ ComponentKitTests/CKBuildTriggerTests.mm
@@ -0,0 +1,81 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#import <XCTest/XCTest.h>
+
+#import <ComponentKit/CKBuildComponent.h>
+#import <ComponentKit/CKComponent.h>
+#import <ComponentKit/CKComponentScopeRootFactory.h>
+#import <ComponentKit/CKComponentScopeRoot.h>
+
+@interface CKBuildTriggerTests : XCTestCase
+@end
+
+@implementation CKBuildTriggerTests
+
+static auto makeScopeRoot(BOOL empty = NO) -> CK::NonNull<CKComponentScopeRoot *> {
+  const auto scopeRoot = CKComponentScopeRootWithDefaultPredicates(nil, nil);
+
+  if (empty == NO) {
+    [scopeRoot setRootComponent:[CKComponent new]];
+  }
+
+  return scopeRoot;
+}
+
+static auto nonEmptyStateUpdates() -> CKComponentStateUpdateMap {
+  CKComponentStateUpdateMap map;
+  map[[CKComponentScopeHandle new]] = {};
+  return map;
+}
+
+- (void)testBuildTriggerNoneWhenEmptyScopeRoot
+{
+  const auto trigger = CKBuildComponentTrigger(makeScopeRoot(YES), nonEmptyStateUpdates(), YES, YES);
+  XCTAssertEqual(trigger, CKBuildTriggerNone);
+}
+
+- (void)testBuildTriggerStateUpdate
+{
+  const auto trigger = CKBuildComponentTrigger(makeScopeRoot(), nonEmptyStateUpdates(), NO, NO);
+  XCTAssertEqual(trigger, CKBuildTriggerStateUpdate);
+}
+
+- (void)testBuildTriggerForcePropsUpdate
+{
+  const auto trigger = CKBuildComponentTrigger(makeScopeRoot(), {}, NO, YES);
+  XCTAssertEqual(trigger, CKBuildTriggerPropsUpdate);
+}
+
+- (void)testBuildTriggerMustBePropsUpdate
+{
+  const auto trigger = CKBuildComponentTrigger(makeScopeRoot(), {}, NO, NO);
+  XCTAssertEqual(trigger, CKBuildTriggerPropsUpdate);
+}
+
+- (void)testBuildTriggerStatePropsUpdate
+{
+  const auto trigger = CKBuildComponentTrigger(makeScopeRoot(), nonEmptyStateUpdates(), NO, YES);
+  XCTAssertEqual(trigger, CKBuildTriggerStateUpdate | CKBuildTriggerPropsUpdate);
+}
+
+- (void)testBuildTriggerEnvironmentUpdate
+{
+  const auto trigger = CKBuildComponentTrigger(makeScopeRoot(), {}, YES, NO);
+  XCTAssertEqual(trigger, CKBuildTriggerEnvironmentUpdate);
+}
+
+- (void)testBuildTriggerAll
+{
+  const auto trigger = CKBuildComponentTrigger(makeScopeRoot(), nonEmptyStateUpdates(), YES, YES);
+  XCTAssertEqual(trigger, CKBuildTriggerPropsUpdate | CKBuildTriggerStateUpdate | CKBuildTriggerEnvironmentUpdate);
+}
+
+@end
