3ee2ebdcffa742797547dc04a9a9c92706f97fcc
Hugo Cuvillier <cuva@fb.com>
2020-09-21 09:42:12 -0700

    Adds CKTextSwift target
    Summary: Adds a swift wrapper for `CKTextSwift`. Mostly used for testing purposes in `WildeGuess`
    
    Reviewed By: kevin0571
    
    Differential Revision: D23807454
    
    fbshipit-source-id: ed5c45a746e1538d2cce5e315c42b6ea10ead21a

---
+++ CKTextSwift/CKTextSwift.swift
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+import UIKit
+import ComponentKit
+import ComponentTextKit
+
+// Re-export the type to avoid importing ComponentTextKit directly and ambiguities in type lookup
+public typealias LabelComponent = ComponentTextKit.LabelComponent
