e8f01c84c3fe12f92f662bc93c33a2483c2ce34a
Hugo Cuvillier <cuva@fb.com>
2020-10-01 11:41:58 -0700

    Adds `ComponentView` & `WrapperComponentView`
    Summary:
    Two values reprenting `CKComponent` & `CKCompositeComponent` in `CKSwift`.
    
    Strongly typed example:
    
    ```
    let imageView = ComponentView<UIImageView> {
      (\.image, image)
      (\.contentMode, .scaleAspectFill)
      (\.clipsToBounds, true)
    }
    ```
    
    Reviewed By: kevin0571
    
    Differential Revision: D23838450
    
    fbshipit-source-id: f3f338131d419c0341956311dc1e2f48b7711807

---
+++ CKSwift/ComponentView.swift
@@ -0,0 +1,108 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+import Foundation
+import UIKit
+import ComponentKit
+
+#if swift(>=5.3)
+
+// MARK: ComponentView
+
+/// `ComponentView` is the `View` based `Component` primitive. Will become a leaf component.
+/// As opposed to `Component`, it has the benefit to being a value type.
+public struct ComponentView<UIViewType: UIView> : View, ViewAttributeAssignable {
+  public init() { }
+
+  public typealias AttributeBuilder = ViewConfigurationAttributeBuilder<UIViewType>
+  public init(@AttributeBuilder attributes: () -> [AttributeBuilder.Directive]) {
+    consume(attributes)
+  }
+
+  // MARK: ViewAttributeAssignable
+
+  public var attributes: [ViewConfiguration.Attribute<UIViewType>] = []
+  public var layerAttributes: [ViewConfiguration.LayerAttribute] = []
+
+  // MARK: ComponentInflatable
+
+  public func inflateComponent(with model: SwiftComponentModel?) -> Component {
+    var result : Component {
+      if model?.requiresSwiftComponent == true {
+        return CKSwiftComponent(
+          swiftView: viewConfiguration.viewConfiguration,
+          swiftSize: nil,
+          child: nil,
+          model: model?.toSwiftBridge()
+        )
+      } else {
+        return Component(view: viewConfiguration)
+      }
+    }
+
+    return result.animated(model?.animations)
+  }
+}
+
+/// `WrapperComponentView` is the `View` based `CompositeComponent` primitive.
+/// As opposed to `CompositeComponent`, it has the benefit to being a value type.
+public struct WrapperComponentView<UIViewType: UIView> : View, ViewAttributeAssignable {
+  let component: () -> Component
+
+  public typealias AttributeBuilder = ViewConfigurationAttributeBuilder<UIViewType>
+  public init(
+    @AttributeBuilder attributes: () -> [AttributeBuilder.Directive],
+    @ComponentBuilder component: @escaping () -> Component) {
+    self.component = component
+    consume(attributes)
+  }
+
+  // MARK: ViewAttributeAssignable
+
+  public var attributes: [ViewConfiguration.Attribute<UIViewType>] = []
+  public var layerAttributes: [ViewConfiguration.LayerAttribute] = []
+
+  // MARK: ComponentInflatable
+
+  public func inflateComponent(with model: SwiftComponentModel?) -> Component {
+    var result : Component {
+      if model?.requiresSwiftComponent == true {
+        return CKSwiftComponent(
+          swiftView: viewConfiguration.viewConfiguration,
+          swiftSize: nil,
+          child: self.component(),
+          model: model?.toSwiftBridge()
+        )
+      } else {
+        return CompositeComponent(
+          view: viewConfiguration,
+          component: self.component())
+      }
+    }
+
+    return result.animated(model?.animations)
+  }
+}
+
+fileprivate extension ViewAttributeAssignable {
+  mutating func consume(_ attributes: () -> [ViewConfigurationAttributeBuilder<UIViewType>.Directive]) {
+    attributes()
+      .forEach {
+        switch $0 {
+        case let .attribute(attribute):
+          self.attributes.append(attribute)
+        case let .layerAttribute(attribute):
+          self.layerAttributes.append(attribute)
+        }
+    }
+  }
+}
+
+#endif
