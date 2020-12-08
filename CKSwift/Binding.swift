d9873c49fa5cebc71fddc19df0803041a2eaa510
Ruslan Serebriakov <rsrbk@fb.com>
2020-12-08 07:20:24 -0800

    Allowing @Binding to be mapped T80071972
    Summary: Added another initializer for the property wrapper Binding in order to set custom getter/setter when we want to bind a sub-property of a state
    
    Reviewed By: cuva
    
    Differential Revision: D25332183
    
    fbshipit-source-id: 39e2cef3aa868fc9d025d82cfeefb79f5a6e1d0d

--- CKSwift/Binding.swift
+++ CKSwift/Binding.swift
@@ -13,28 +13,69 @@ import ComponentKit
 
 #if swift(>=5.3)
 
+@dynamicMemberLookup
 @propertyWrapper
 public struct Binding<Value> {
-  private let state: State<Value>
+	private enum Store {
+		case state(State<Value>)
+		case closures(valueProvider: () -> Value, valueModifier: (Value) -> Void)
+	}
 
-  public init(state: State<Value>) {
-    self.state = state
-  }
+	private let store: Store
 
-  public var wrappedValue: Value {
-    get { state.wrappedValue }
-    nonmutating set { state.wrappedValue = newValue }
-  }
+	public init(state: State<Value>) {
+		store = .state(state)
+	}
 
-  public var projectedValue: Binding<Value> {
-    self
-  }
+	private init(closures: (valueProvider: () -> Value, valueModifier: (Value) -> ())) {
+		store = .closures(valueProvider: closures.valueProvider, valueModifier: closures.valueModifier)
+	}
+
+	public var wrappedValue: Value {
+		get {
+			switch store {
+			case .state(let state):
+				return state.wrappedValue
+			case .closures(let valueProvider, _):
+				return valueProvider()
+			}
+		}
+		nonmutating set {
+			switch store {
+			case .state(let state):
+				state.wrappedValue = newValue
+			case .closures(_, let valueModifier):
+				valueModifier(newValue)
+			}
+		}
+	}
+
+	public var projectedValue: Binding<Value> {
+		self
+	}
+
+	public subscript<T>(dynamicMember keyPath: WritableKeyPath<Value, T>) -> Binding<T> {
+		switch store {
+		case .state(let state):
+			return Binding<T>(closures: ({ state.wrappedValue[keyPath: keyPath] }, { state.wrappedValue[keyPath: keyPath] = $0 }))
+		case .closures(_, _):
+			return Binding<T>(closures: ({ self.wrappedValue[keyPath: keyPath] }, { self.wrappedValue[keyPath: keyPath] = $0 }))
+		}
+	}
 }
 
+
 extension Binding : Equatable where Value : Equatable {
-  static public func ==(lhs: Binding, rhs: Binding) -> Bool {
-    return lhs.state == rhs.state
-  }
+	static public func ==(lhs: Binding, rhs: Binding) -> Bool {
+		switch (lhs.store, rhs.store) {
+		case (.state(let lhsState), .state(let rhsState)):
+			return lhsState == rhsState
+		case (.closures(_, _), .closures(_, _)):
+			return lhs.wrappedValue == rhs.wrappedValue
+		default:
+			return false
+		}
+	}
 }
 
 #endif
