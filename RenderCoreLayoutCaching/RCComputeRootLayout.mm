09c9009ef2edaf0736a5d0aa5f296d1c22955c01
Shubho Sadhu <shubhos@fb.com>
2021-03-08 13:44:47 -0800

    Revert D26884118: Include parent size in cache key hash
    Differential Revision:
    D26884118
    
    Original commit changeset: c7bcf82b65f6
    
    fbshipit-source-id: 9eabea2a6968243d61b548588146df4b5eb2eb9b

--- RenderCoreLayoutCaching/RCComputeRootLayout.mm
+++ RenderCoreLayoutCaching/RCComputeRootLayout.mm
@@ -28,25 +28,10 @@ struct RCLayoutCacheKey {
 
 namespace std {
   template <>
-  struct hash<CGSize> {
-    size_t operator ()(const CGSize &size) noexcept {
-      const auto hashes = {
-        std::hash<CGFloat>()(size.width),
-        std::hash<CGFloat>()(size.height),
-      };
-      return RCIntegerArrayHash(hashes.begin(), hashes.size());
-    }
-  };
-
-  template <>
   struct hash<RCLayoutCacheKey> {
     size_t operator()(const RCLayoutCacheKey &key) const noexcept
     {
-      const auto hashes = {
-        key.constrainingSize.hash(),
-        std::hash<CGSize>()(key.parentSize)
-      };
-      return RCIntegerArrayHash(hashes.begin(), hashes.size());
+      return key.constrainingSize.hash();
     }
   };
 }
