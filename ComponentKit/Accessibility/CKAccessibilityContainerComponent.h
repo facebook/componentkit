df0a0531cd422db7fa45aa6f8eafcafd9daac1f1
Ivan Misuno <ivanmisuno@fb.com>
2021-03-11 11:06:14 -0800

    Fix nullability warning
    Summary: Even though the typedef was in `NS_ASSUME_NONNULL_BEGIN` block, a nullability warning was generated.
    
    Reviewed By: cuva
    
    Differential Revision: D26979006
    
    fbshipit-source-id: 705d8c1a4d0cdfd329ebb0eea1d50a929d6ee3be

--- ComponentKit/Accessibility/CKAccessibilityContainerComponent.h
+++ ComponentKit/Accessibility/CKAccessibilityContainerComponent.h
@@ -17,18 +17,20 @@ NS_ASSUME_NONNULL_BEGIN
  * the container view. The view is mounted in the hierarchy when the factory
  * function is called.
  */
-typedef NSArray<UIAccessibilityElement *> *(^CKAccessibilityElementsFactory)(
-  UIView *containerView);
+typedef NSArray<UIAccessibilityElement *> * _Nonnull (^CKAccessibilityElementsFactory)(
+  UIView * _Nonnull containerView);
 
 /**
  * Creates a wrapper component which always mounts an accessibility
- * container view (not accessible by itself) and exposes an array of
- * `accessibilityElements`, thus enabling accessibility aggregation.
+ * container view which exposes an array of `accessibilityElements`.
+ * The view is not an accessibility element by itself.
  *
- * Unlike setting `.accessibilityAggregateAttributes()` via the Builder,
+ * Unlike using `.accessibilityAggregateAttributes()` Builder attribute,
  * this class allows call sites to completetely control how accessibility
  * elements are created via the provided factory function.
  */
-CKComponent *CKAccessibilityContainerComponentWrapper(CKComponent * _Nullable component, CKAccessibilityElementsFactory factory);
+CKComponent *CKAccessibilityContainerComponentWrapper(
+  CKComponent * _Nullable component,
+  CKAccessibilityElementsFactory factory);
 
 NS_ASSUME_NONNULL_END
